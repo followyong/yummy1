7b291a122218630295a6b7b39b049b828537141a
followyong <947690826@qq.com>
2017-07-24 12:01:19 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './App.css';
+import {BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom'
+import Home from './ui/pages/Home/Home'
+import Signup from './ui/pages/Signup/Signup'
+import Sidebar from './ui/shared/Sidebar/Sidebar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          app
+        <Router>
+          <div>
+            <Sidebar />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/signup' exact component={Signup} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
